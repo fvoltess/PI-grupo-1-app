9c34faf5f2015cb1d4490a18d27cc9d70ea464e7
bautistagorchs <bautista.gorchs@gmail.com>
2025-11-04 09:49:26 -0300

    corregir pequeños errores

--- database/models/User.js
+++ database/models/User.js
@@ -1,54 +1,56 @@
-module.exports = function (sequelize, DataTypes){
-    const alias = "User";
+module.exports = function (sequelize, DataTypes) {
+  const alias = "User";
 
-    const columns = {
-        id: {
-            type: DataTypes.INTEGER.UNSIGNED,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        name: {
-            type: DataTypes.STRING(250),
-        },
-        email: {
-            type: DataTypes.STRING(250),
-        },
-        password: {
-            type: DataTypes.STRING(250),
-        },
-        profilePicture: {
-            type: DataTypes.STRING(250),
-        },
-        createdAt: {
-            type: DataTypes.DATE,
-        },
-        updatedAt: {
-            type: DataTypes.DATE,
-        },
-        deletedAt: {
-            type: DataTypes.DATE,
-        },
-    };
+  const columns = {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(250),
+    },
+    email: {
+      type: DataTypes.STRING(250),
+    },
+    password: {
+      type: DataTypes.STRING(250),
+    },
+    profilePicture: {
+      type: DataTypes.STRING(250),
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+    },
+  };
 
-    const config = {
-        tableName: "users",
-        timestamps: true,
-        underscored: false,
-    };
+  const config = {
+    tableName: "users",
+    timestamps: true,
+    underscored: false,
+  };
 
-    const User = sequelize.define(alias, columns, config)
+  const User = sequelize.define(alias, columns, config);
 
-    User.associate = function (models) {
-        User.hasMany(models.Comment, {
-            as: "comments",
-            foreignKey: "",
-            timestamps: true,
-        })
+  User.associate = function (models) {
+    User.hasMany(models.Comment, {
+      as: "comments",
+      foreignKey: "",
+      timestamps: true,
+    });
 
-        User.hasMany(models.Product, {
-            as: "products",
-            foreignKey: "",
-            timestamps: true,
-        })
-    }
-}
+    User.hasMany(models.Product, {
+      as: "products",
+      foreignKey: "",
+      timestamps: true,
+    });
+  };
+
+  return User;
+};
