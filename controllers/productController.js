939a7086913e94323127021ae9dbd5c036815c41
bautistagorchs <bautista.gorchs@gmail.com>
2025-11-10 16:14:01 -0300

    Eliminar código de depuración, ajustar rutas de productos y mejorar la estructura del formulario de comentarios.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,14 +1,16 @@
 const db = require("../database/models");
 const products = db.Product;
+const comments = db.Comment;
 const op = db.Sequelize.Op;
 
 const controller = {
   product: function (req, res) {
     let id = req.params.id;
 
-    products.findByPk(id, {
-      include: [{ association: "comments" }, { association: "user" }],
-    })
+    products
+      .findByPk(id, {
+        include: [{ association: "comments" }, { association: "user" }],
+      })
       .then(function (resultado) {
         if (resultado) {
           // return res.send(resultado)
@@ -22,47 +24,60 @@ const controller = {
       });
   },
   addProduct: function (req, res) {
-
-    return res.send("test")
+    return res.send("test");
     if (!req.session.user) {
       return res.redirect("/users/login");
     }
     res.render("product-add", { title: "Add Product" });
   },
   storeProduct: function (req, res) {
-    products.create({
-      image : req.body.imagen,
-      name: req.body.nombre,
-      description : req.body.descripcion,
-    })
-      .then(function() {
-        res.redirect('/')
+    products
+      .create({
+        image: req.body.imagen,
+        name: req.body.nombre,
+        description: req.body.descripcion,
+      })
+      .then(function () {
+        res.redirect("/");
       })
       .catch(function (error) {
-        res.render("error")
+        res.render("error");
       });
   },
   editProduct: function (req, res) {
     if (!req.session.user) {
       return res.redirect("/users/login");
     }
-    res.render("product-edit", { title: "Edit Product" });
+    comments
+      .create({
+        productId: req.params.id,
+        userId: req.session.user.id,
+        comment: req.body.comment,
+      })
+      .then(function () {
+        res.redirect(`/products/id/${req.params.id}`);
+      })
+      .catch(function (error) {
+        res.render("error");
+      });
+
+    // res.render("product-edit", { title: "Edit Product" });
   },
   searchProduct: function (req, res) {
-
-    products.findAll({
-      where: [{name: {[op.like]: "%"+req.query.search+"%"}}],
-      include: [{all: true, nested: true}] ,
-    })
+    products
+      .findAll({
+        where: [{ name: { [op.like]: "%" + req.query.search + "%" } }],
+        include: [{ all: true, nested: true }],
+      })
       .then(function (productos) {
         //return res.send(productos)
-        res.render("search-results", {productos: productos});
+        res.render("search-results", { productos: productos });
       })
-      .catch (function (error) {
-        return res.send("error")
+      .catch(function (error) {
+        return res.send("error");
         //res.render("error")
       });
-  }
+  },
 };
 
 module.exports = controller;
--- routes/products.js
+++ routes/products.js
@@ -5,10 +5,9 @@ const productController = require("../controllers/productController");
 router.get("/id/:id", productController.product);
 
 router.get("/product-add", productController.addProduct);
-router.post("/product-add", productController.storeProduct)
+router.post("/product-add", productController.storeProduct);
 
-router.get("/product-edit", productController.editProduct);
-
+router.post("/product-edit/:id", productController.editProduct);
 router.get("/search-results", productController.searchProduct);
 
 module.exports = router;
